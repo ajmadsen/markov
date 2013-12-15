d78feb172d070358a47200e91c84ebe0c39aacb5
Adam Madsen <madsen.adam@gmail.com>
2013-12-15 04:29:58 -0600

    Prepare all statements; still not seeing the desired performance

--- lib/markov/text_processor.rb
+++ lib/markov/text_processor.rb
@@ -18,25 +18,27 @@ module Markov
       word_id_map = {}
       state_id_map = {}
 
-      chunker.each_with_index do |chunk, idx|
-        tokens = [""] * @chain.rank + tokenizer.tokenize(chunk) + [""]
-        tokens.each_cons(@chain.rank+1) do |parts|
-          parts.map! do |word|
-            word_id = word_id_map[word] ||= db[:words].where(:word => word).get(:id)
-            unless word_id
-              word_id_map[word] = db[:words].insert(:word => word)
+      insert_word = db[:words].prepare(:insert, :insert_word, :word => :$word)
+      select_word = db[:words].where(:word => :$word).prepare(:first, :select_word)
+      insert_state = db[:states].prepare(:insert, :insert_state, :list => :$list)
+      select_state = db[:states].where(:list => :$list).prepare(:first, :select_state)
+      insert_pairing = db[:pairings].prepare(:insert, :insert_pairing, :chain_id => @chain.id, :state_id => :$state_id, :word_id => :$word_id)
+
+      db.transaction do
+        chunker.each_with_index do |chunk, idx|
+          tokens = [""] * @chain.rank + tokenizer.tokenize(chunk) + [""]
+          tokens.each_cons(@chain.rank+1) do |parts|
+            parts.map! do |word|
+              word_id_map[word] ||= (select_word.call(:word => word) || {:id => insert_word.call(:word => word)})[:id]
             end
+
+            state_string = parts[0..-2].join(",")
+            state_id = state_id_map[state_string] ||= (select_state.call(:list => state_string) || {:id => insert_state.call(:list => state_string)})[:id]
+
+            insert_pairing.call(:state_id => state_id, :word_id => parts[-1])
+
+            print "Processing...#{idx}\r" if idx % 100 == 0
           end
-
-          state_string = parts[0..-2].join(",")
-          state_id = state_id_map[state_string] ||= db[:states].where(:list => state_string).get(:id)
-          unless state_id
-            state_id = state_id_map[state_string] = db[:states].insert(:list => state_string)
-          end
-
-          db[:pairings].insert(:chain_id => @chain.id, :state_id => state_id, :word_id => parts[-1])
-
-          print "Processing...#{idx}\r" if idx % 100 == 0
         end
       end
     end
