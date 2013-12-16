8aaf1b55b78a8c60178bab74f702d65fb479b3ce
Adam Madsen <madsen.adam@gmail.com>
2013-12-16 17:08:34 -0600

    Add methods for getting associations and chains

--- lib/markov/database.rb
+++ lib/markov/database.rb
@@ -33,11 +33,20 @@ module Markov
       @db.execute_batch SCHEMA
 
       @select_chain_id = @db.prepare "SELECT id FROM chains WHERE name = ? AND rank = ? LIMIT 1"
+      @select_chains = @db.prepare "SELECT name,rank FROM chains"
       @insert_chain = @db.prepare "INSERT INTO chains(name,rank) VALUES(?,?)"
       @select_word_id = @db.prepare "SELECT id FROM words WHERE word = ? LIMIT 1"
       @insert_word = @db.prepare "INSERT INTO words(word) VALUES(?)"
       @select_state_id = @db.prepare "SELECT id FROM states WHERE list = ? LIMIT 1"
       @insert_state = @db.prepare "INSERT INTO states(list) VALUES(?)"
+      @select_associations = @db.prepare <<-SQL
+        SELECT W.word
+        FROM associations AS A
+        LEFT OUTER JOIN words AS W
+        ON W.id = A.next_state_id
+        WHERE A.state_id = ?
+              AND A.chain_id = ?
+      SQL
       @insert_association = @db.prepare "INSERT INTO associations(chain_id,state_id,next_state_id) VALUES(?,?,?)"
 
       @cache = {
@@ -55,6 +64,10 @@ module Markov
         row = result.next
       end
       @cache[:chain][[name, rank]] = row ? row.first : nil
+    end
+
+    def get_chains
+      @select_chains.execute!
     end
 
     def put_chain(name, rank)
@@ -88,6 +101,15 @@ module Markov
     def put_state(list)
       @insert_state.execute list
       @cache[:state][list] = @db.last_insert_row_id
+    end
+
+    def get_associations(chain, state)
+      associations = @cache[:assoc][[chain, state]]
+      return associations unless associations.nil?
+      @select_associations.execute state, chain do |result|
+        associations = result.entries.flatten
+      end
+      @cache[:assoc][[chain, state]] = associations
     end
 
     def put_association(chain, state, next_state)
