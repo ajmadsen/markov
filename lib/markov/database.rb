ea37c16a516085c8a6974b2c7a5d48d7356fde85
Adam Madsen <madsen.adam@gmail.com>
2013-12-19 03:14:03 -0600

    Clean up SQL syntax; add methods for querying and deleting chains

--- lib/markov/database.rb
+++ lib/markov/database.rb
@@ -4,50 +4,64 @@ module Markov
   class Database
 
     SCHEMA = <<-SQL
-      CREATE TABLE IF NOT EXISTS chains (
-        id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name text NOT NULL UNIQUE,
-        rank integer NOT NULL
+      CREATE TABLE IF NOT EXISTS `chains` (
+        `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `name` text NOT NULL UNIQUE,
+        `rank` integer NOT NULL
       );
 
-      CREATE TABLE IF NOT EXISTS words (
-        id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-        word text NOT NULL UNIQUE
+      CREATE TABLE IF NOT EXISTS `words` (
+        `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `word` text NOT NULL UNIQUE
       );
 
-      CREATE TABLE IF NOT EXISTS states (
-        id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-        list text NOT NULL UNIQUE
+      CREATE TABLE IF NOT EXISTS `states` (
+        `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `list` text NOT NULL UNIQUE
       );
 
-      CREATE TABLE IF NOT EXISTS associations (
-        id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-        chain_id integer REFERENCES chains(id) ON DELETE CASCADE,
-        state_id integer REFERENCES states(id),
-        next_state_id integer REFERENCES words(id)
+      CREATE TABLE IF NOT EXISTS `associations` (
+        `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `chain_id` integer REFERENCES `chains`(`id`) ON DELETE CASCADE,
+        `state_id` integer REFERENCES `states`(`id`),
+        `next_state_id` integer REFERENCES `words`(`id`)
       );
+
+      PRAGMA foreign_keys = ON;
     SQL
 
     def initialize(database)
       @db = SQLite3::Database.new database
       @db.execute_batch SCHEMA
 
-      @select_chain_id = @db.prepare "SELECT id FROM chains WHERE name = ? AND rank = ? LIMIT 1"
-      @select_chains = @db.prepare "SELECT name,rank FROM chains"
-      @insert_chain = @db.prepare "INSERT INTO chains(name,rank) VALUES(?,?)"
-      @select_word_id = @db.prepare "SELECT id FROM words WHERE word = ? LIMIT 1"
-      @insert_word = @db.prepare "INSERT INTO words(word) VALUES(?)"
-      @select_state_id = @db.prepare "SELECT id FROM states WHERE list = ? LIMIT 1"
-      @insert_state = @db.prepare "INSERT INTO states(list) VALUES(?)"
-      @select_associations = @db.prepare <<-SQL
-        SELECT W.word
-        FROM associations AS A
-        LEFT OUTER JOIN words AS W
-        ON W.id = A.next_state_id
-        WHERE A.state_id = ?
-              AND A.chain_id = ?
+      @select_chain_id = @db.prepare "SELECT `id` FROM `chains` WHERE `name` = ? LIMIT 1"
+      @select_chains = @db.prepare "SELECT `name`,`rank` FROM `chains`"
+      @select_chains_with_records = @db.prepare <<-SQL
+        SELECT
+          `chains`.`name`,
+          `chains`.`rank`,
+          (
+            SELECT COUNT(*)
+            FROM `associations`
+            WHERE `chain_id` = `chains`.`id`
+          ) AS `associated_records`
+        FROM `chains`
       SQL
-      @insert_association = @db.prepare "INSERT INTO associations(chain_id,state_id,next_state_id) VALUES(?,?,?)"
+      @insert_chain = @db.prepare "INSERT INTO `chains`(`name`,`rank`) VALUES(?,?)"
+      @delete_chain = @db.prepare "DELETE FROM `chains` WHERE `name` = ?"
+      @select_word_id = @db.prepare "SELECT `id` FROM `words` WHERE `word` = ? LIMIT 1"
+      @insert_word = @db.prepare "INSERT INTO `words`(`word`) VALUES(?)"
+      @select_state_id = @db.prepare "SELECT `id` FROM `states` WHERE `list` = ? LIMIT 1"
+      @insert_state = @db.prepare "INSERT INTO `states`(`list`) VALUES(?)"
+      @select_associations = @db.prepare <<-SQL
+        SELECT `words`.`word`
+        FROM `associations` AS `A`
+        LEFT JOIN `words`
+        ON `words`.`id` = `A`.`next_state_id`
+        WHERE `A`.`state_id` = ?
+              AND `A`.`chain_id` = ?
+      SQL
+      @insert_association = @db.prepare "INSERT INTO `associations`(`chain_id`,`state_id`,`next_state_id`) VALUES(?,?,?)"
 
       @cache = {
         :chain => {},
@@ -57,7 +71,7 @@ module Markov
       }
     end
 
-    def get_chain(name, rank)
+    def get_chain(name)
       row = @cache[:chain][[name, rank]]
       return row unless row.nil?
       @select_chain_id.execute name, rank do |result|
@@ -66,13 +80,25 @@ module Markov
       @cache[:chain][[name, rank]] = row ? row.first : nil
     end
 
-    def get_chains
-      @select_chains.execute!
+    def get_chains(with_records=false)
+      stmt = with_records ? @select_chains_with_records : @select_chains
+      if block_given?
+        stmt.execute do |result|
+          r = nil; yield r until (r = result.next).nil?
+        end
+      else
+        stmt.execute!
+      end
     end
 
     def put_chain(name, rank)
       @insert_chain.execute name, rank
       @cache[:chain][[name, rank]] = @db.last_insert_row_id
+    end
+
+    def delete_chain(name)
+      @delete_chain.execute name
+      @db.changes
     end
 
     def get_word(word)
@@ -121,6 +147,10 @@ module Markov
       @db.transaction do |db|
         yield db
       end
+    end
+
+    def vacuum
+      @db.execute "VACUUM"
     end
   end
 end
